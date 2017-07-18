6c9aba8294987f6e3b1434fabe8a6cbf20661dd2
Aaron Mondshine <aaron.mondshine@gmail.com>
2017-07-18 08:34:29 -0700

    Start Auth, finish db migration, user model, and application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,32 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :signed_in?, :current_user
+  before_action :require_logged_in #correct?
+
+  private
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def login
+    @current_user = user
+    session[:session_token] = user.reset_token!
+  end
+
+  def logout
+    current_user.try(:reset_token!)
+    session[:session_token] = nil
+  end
+
+  def require_logged_in
+    render json: ['invalid credentials'], status: 401 if !current_user
+  end
 end
+
+#has comments
