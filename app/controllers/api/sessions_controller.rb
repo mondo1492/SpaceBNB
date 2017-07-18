b27eadda9d6719fd06f0c5fdd201dbd4f4c22954
Aaron Mondshine <aaron.mondshine@gmail.com>
2017-07-18 08:47:06 -0700

    Finish users and sessions controller. Add static pages controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,30 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_cred(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user
+      login!(@user)
+      render '/api/user/show'
+    else
+      render(
+        json: ["Invalid Username or Password"],
+        status: 401
+      )
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout!
+      render '/api/user/show'
+    else
+      render(
+        json: ["Nobody signed in"],
+        status: 404
+      )
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    resource :user, only: %i(create)
+    resource :session, only: %i(create destroy)
+  end
 end
