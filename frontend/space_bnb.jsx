9311e95cbad147eae00c218d3d7ebb907fda84c8
Aaron Mondshine <aaron.mondshine@gmail.com>
2017-07-18 12:10:47 -0700

    add App and root components

--- frontend/space_bnb.jsx
+++ frontend/space_bnb.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 ///testing
 import { login, logout, signup } from './util/session_api_util';
 ///
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  window.store = store;
+  window.dispatch = store.dispatch;
+  window.getState = store.getState;
   window.login = login;
   window.signup = signup;
   window.logout = logout;
